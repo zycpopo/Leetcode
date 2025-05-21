b473b3bef15b5830d4b80bd9b513215ee8669ab3
purple-onion <3368185928@qq.com>
2025-05-21 19:43:25 +0800

    cpp

---
+++ 557.反转字符串中的单词-iii.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=557 lang=cpp
+ *
+ * [557] 反转字符串中的单词 III
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n=s.size();
+        int i=0;
+
+        while(i<n)
+        {
+            int start=i;
+            while(i<n&&s[i]!=' ')
+            {
+                i++;
+            }
+            reverse(s.begin() + start, s.begin() + i);
+
+            while(i<n&&s[i]==' ')
+            {
+                i++;
+            }
+        }
+    return s;
+    }
+};
+// @lc code=end
+
