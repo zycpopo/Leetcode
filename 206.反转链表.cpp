5b3e3ed61163fb253ec778f89bfe1fb9791b2e71
purple-onion <3368185928@qq.com>
2025-06-05 22:59:31 +0800

    递归

---
+++ 206.反转链表.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head==nullptr||head->next==nullptr) return head;
+
+         ListNode*newhead =reverseList(head->next);
+        
+         head->next->next=head;
+         head->next=nullptr;
+
+         return newhead;
+    }
+};
+// @lc code=end
+
