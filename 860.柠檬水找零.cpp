5083caf9f933f28aaec2bdff840d79cf942e1d9f
purple-onion <3368185928@qq.com>
2025-06-21 21:34:09 +0800

    贪心

---
+++ 860.柠檬水找零.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=860 lang=cpp
+ *
+ * [860] 柠檬水找零
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0,ten=0;
+        for(auto x : bills)
+        {
+            if(x==5) five++;
+            else if(x==10)
+            {
+                if(five==0) return false;
+                five--;ten++;
+            }
+            else{
+                if(ten&&five)
+                {
+                    ten--;
+                    five--;
+                }
+                else if(five>=3)
+                {
+                     five-=3;
+                }
+                else return false;
+            }
+        }
+        return true;
+    }
+};
+// @lc code=end
+
