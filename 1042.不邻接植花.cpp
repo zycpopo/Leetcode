45a6c3a9c81d75b8c487e6de37811fde1041f63a
purple-onion <3368185928@qq.com>
2025-07-12 22:36:12 +0800

    深搜

---
+++ 1042.不邻接植花.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1042 lang=cpp
+ *
+ * [1042] 不邻接植花
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> gardenNoAdj(int n, vector<vector<int>>& paths) {
+        
+    }
+};
+// @lc code=end
+
