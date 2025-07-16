2daf7a024c1157cc3618dcb4f501198eecf88086
purple-onion <3368185928@qq.com>
2025-07-16 18:59:28 +0800

    双指针

--- 15.三数之和.cpp
+++ 15.三数之和.cpp
@@ -8,7 +8,34 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
+        vector<vector<int>> ret;
+
+        sort(nums.begin(),nums.end());
+
+        int n=nums.size();
+        if (n < 3) return ret;
+
+        for(int i=0;i<n;)
+        {
+            if(nums[i]>0) break;
+            int left=i+1,right=n-1,target=-nums[i];
+            while(left<right)
+            {
+                int sum=nums[left]+nums[right];
+                if(sum>target) right--;
+                else if(sum<target) left++;
+                else{
+                    ret.push_back({nums[i],nums[left],nums[right]});
+                    left++,right--;
+
+                    while(left<right&&nums[left]==nums[left-1]) left++;
+                    while(left<right&&nums[right]==nums[right+1]) right--;
+                }
+            }
+            i++;
+            while(i<n-2&&nums[i]==nums[i-1]) i++;
+        }
+        return ret;
     }
 };
 // @lc code=end
