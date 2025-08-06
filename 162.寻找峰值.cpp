055c9681c7f6e3f8281d42623bf2413a50de5292
purple-onion <3368185928@qq.com>
2025-08-06 22:03:34 +0800

    二分查找

---
+++ 162.寻找峰值.cpp
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=162 lang=cpp
+ *
+ * [162] 寻找峰值
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int left=0,right=nums.size()-1;
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            if(nums[mid]>nums[mid+1]) right=mid;
+            else left=mid+1;
+        }
+        return left;
+    }
+};
+// @lc code=end
+
