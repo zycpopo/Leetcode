600d7531ebbe8af560defc63937e817618c63f0b
purple-onion <3368185928@qq.com>
2025-09-21 19:11:47 +0800

    二分查找

---
+++ 1208.尽可能使字符串相等.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1208 lang=cpp
+ *
+ * [1208] 尽可能使字符串相等
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        
+    }
+};
+// @lc code=end
+
