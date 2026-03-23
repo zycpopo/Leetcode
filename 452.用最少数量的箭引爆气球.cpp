6f54f0b423f43c8696a4726cc783ba415c5b3964
purple-onion <3368185928@qq.com>
2026-03-23 09:10:40 -0400

    贪心

--- 452.用最少数量的箭引爆气球.cpp
+++ 452.用最少数量的箭引爆气球.cpp
@@ -7,8 +7,24 @@
 // @lc code=start
 class Solution {
 public:
-    int findMinArrowShots(vector<vector<int>>& points) {
-        
+    int findMinArrowShots(vector<vector<int>>& arr) {
+        sort(arr.begin(), arr.end());
+
+        int right=arr[0][1];
+        int ret=1;
+        for(int i=1;i < arr.size();i++)
+        {
+            int a = arr[i][0],b = arr[i][1];
+            if(a <= right)
+            {
+                right = min(right,b);
+            }
+            else{
+                ret++;
+                right = b;
+            }
+        }
+        return ret;
     }
 };
 // @lc code=end
