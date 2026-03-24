6f966f631d67078848b6bdf89cccf0726a756bf8
purple-onion <3368185928@qq.com>
2026-03-24 07:23:05 -0400

    贪心

--- 553.最优除法.cpp
+++ 553.最优除法.cpp
@@ -8,7 +8,25 @@
 class Solution {
 public:
     string optimalDivision(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1) {
+            return to_string(nums[0]);
+        }
+
+        if (n == 2) {
+            return to_string(nums[0]) + "/" + to_string(nums[1]);
+        }
+
+        string result = to_string(nums[0]) + "/(" + to_string(nums[1]);
         
+        for (int i = 2; i < n; ++i) {
+            result += "/" + to_string(nums[i]);
+        }
+        
+        result += ")";
+        
+        return result;
     }
 };
 // @lc code=end
