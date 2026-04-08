470186b1e67a93916fa2fe496a4d40ba7bfec98a
purple-onion <3368185928@qq.com>
2026-04-08 05:11:26 -0400

    动态规划

--- 509.斐波那契数.cpp
+++ 509.斐波那契数.cpp
@@ -7,11 +7,44 @@
 // @lc code=start
 class Solution {
 public:
-    int fib(int n) {
+    /*int fib(int n) {
         if(n==0) return 0;
         if(n==1) return 1;
         return fib(n-1)+fib(n-2);
+    }*/
+
+    //T(n),S(1)
+    /*int fib(int n)
+    {
+        if(n<=1) return n;
+        int dp[2];
+        int result=0;
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            result=dp[1]+dp[0];
+            dp[0]=dp[1];
+            dp[1]=result;
+        }
+        return result;
+    }*/
+
+    //T(n),S(n)
+    int fib(int n)
+    {
+        if(n<=1) return n;
+        vector<int> dp(n+1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
     }
+
 };
 // @lc code=end
 
